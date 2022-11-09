c666611f9e6cc7db5e79320e62e795e819fe866e
Abeeb Ahmed <abeebahmed@gmail.com>
2022-11-09 12:28:15 +0100

    route path edit

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ function App() {
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Home />} />
-        <Route path="/lists" element={<Lists />} />
+        <Route path="/hotels" element={<Lists />} />
       </Routes>
     </BrowserRouter>
   );
